09ccfaf3246a851f73092e0f268346a346c63059
sami jaghouar <sami.jaghouar@hotmail.fr>
2021-11-22 17:00:48 +0100

    initial web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    // GET /info/ => 200 OK with body "Belote Libre"
+    let root = warp::path("info")
+      .map(|| format!("Belote libre"));
+
+
+    warp::serve(root)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
